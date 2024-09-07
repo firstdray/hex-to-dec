5be8e755dce4a811a46814868887cc3a1effc0a5
firstdray <142772338+firstdray@users.noreply.github.com>
2024-09-07 10:14:48 +0400

    Add files via upload

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Enter hex number or 'stop' to exit:")
+
+	var input string
+
+	for {
+		fmt.Scanln(&input)
+
+		input = strings.ToLower(input)
+
+		if input == "stop" {
+			break
+		}
+
+		i := new(big.Int)
+
+		if _, ok := i.SetString(processHex(input), 16); !ok {
+			fmt.Println("Invailed hexdecimal number!")
+			continue
+		}
+
+		fmt.Println(i)
+	}
+}
+
+func processHex(hexStr string) string {
+	return strings.TrimPrefix(hexStr, "0x")
+}
